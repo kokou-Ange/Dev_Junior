8e816645e458ff9a1b999208806a017e04ecca4f
Kokou Ange Julien SEGLA <kokouecom@gmail.com>
2026-08-24 21:58:27 +0200

    Add initial project files including HTML, CSS, and JavaScript for sign-in and sign-up forms, as well as a main index page and contact styles
    - Created script.js for basic JavaScript functionality
    - Added signin.html and signin.css for user authentication interface
    - Implemented signup.html and signup.css for user registration
    - Developed index.html as the main landing page with sections for profile, skills, experiences, and projects
    - Introduced contacts.css for styling contact information
    - Included an image for profile display

---
+++ Javascript/script.js
@@ -0,0 +1,43 @@
+let nom = "SEGLA";
+let prenom = "Kokou Ange Julien";
+let nombre1 = 12;
+let nombre2 = 20;
+let nombre3 = nombre1 + nombre2;
+let age = nombre3;
+let BaseDeDonne = ["Jean","Kokou","Martin","Sariou"]
+// let userAge = prompt("Quel est votre âge ?", "Entrez votre âge ici");
+// let couleur = prompt("Quelle est la couleur du feu?");
+
+// console.log(nombre3);
+// console.log("Bonjour je suis " + prenom + " " + nom + " et j'ai " + age + " ans.");
+// console.log("Votre age est " + userAge + " ans.");
+console.log(BaseDeDonne.length);
+
+
+// switch(couleur) {
+//     case "rouge":
+//         alert("Vous devez vous arrêter.")
+//         break;
+//     case "jaune":
+//         alert("Vous devez ralentir.")
+//         break;
+//     case "vert":
+//         alert("Vous pouvez passer.")
+//         break;
+//     default:
+//         break;
+// }
+
+// if(userAge >= 1 && userAge <= 10){
+//     console.log("Vous êtes un enfant.");
+//     alert("Vous êtes un enfant.");
+// }else if(userAge > 10 && userAge <= 17){
+//     console.log("Vous êtes un adolescent.");
+//     alert("Vous êtes un adolescent.");
+// }else if(userAge >= 17 && userAge <= 40){
+//     console.log("Vous êtes un adulte.");
+//     alert("Vous êtes un adulte.");
+// }else{
+//     console.log("Vous êtes un senior.");
+//     alert("Vous êtes un senior.");
+// }
